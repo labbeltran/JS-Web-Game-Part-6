5d60821c3c847bb8e86b9348e282715d49d1071d
labbeltran <labbeltran@gmail.com>
2023-11-29 18:07:01 -0500

    hmwk day 6

--- move.js
+++ move.js
@@ -36,16 +36,16 @@ function move(element) {
         document.addEventListener('keydown', function(e){
             if(e.repeat) return;
         
-            if(e.key === 'ArrowLeft'){
+            if(e.key === 'a'){
                 direction = 'west'
             }
-            if(e.key === 'ArrowUp'){
+            if(e.key === 'w'){
                 direction = 'north'
             }
-            if(e.key === 'ArrowRight'){
+            if(e.key === 'd'){
                 direction = 'east'
             }
-            if(e.key === 'ArrowDown'){
+            if(e.key === 's'){
                 direction = 'south'
             }
             callback(direction)
