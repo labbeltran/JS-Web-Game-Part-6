44c7db8b731cbe307f411c1aae0299d6bb635a0f
labbeltran <labbeltran@gmail.com>
2023-11-29 18:08:43 -0500

    hmwk day 6- had trouble adding collision detection; also, images dissapear after implementing promise code

--- index.js
+++ index.js
@@ -26,3 +26,32 @@ move(newImage('assets/well.png')).to(500, 575)
 move(newItem('assets/sword.png')).to(500, 555)
 move(newItem('assets/shield.png')).to(165, 335)
 move(newItem('assets/staff.png')).to(600, 250)
+
+
+
+// Function to check for 2D collision between two entities
+
+// function isCollision(npc, sword) {
+//     const sword = (x: 500, y: 555, width: 100, height: 100)
+//     const pillar = (x: 350, y: 250, width: 100, height: 100)
+
+//     return (
+//       npc.x < sword.x + sword.width &&
+//       npc.x + npc.width > sword.x &&
+//       npc.y < sword.y + sword.height &&
+//       npc.y + npc.height > sword.y
+//     );
+//   }
+  
+  // Example usage
+
+// const sword = (x: 500, y: 555, width: 100, height: 100)
+// const pillar = (x: 350, y: 250, width: 100, height: 100))
+
+  
+//   if (isCollision(npc, sword)) {
+//     console.log('Collision detected!');
+//   } else {
+//     console.log('No collision.');
+//   }
+  
